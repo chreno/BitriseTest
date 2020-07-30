a8745b7654492f5ab8b9c750f8d49fe5c09cbc22
Tamas Chrenoczy-Nagy <chreno@gmail.com>
2020-07-30 15:26:54 +0200

    Add files via upload

---
+++ TestApp/TestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestApp
+//
+//  Created by Tamas Chrenoczy-Nagy on 30/07/2020.
+//  Copyright © 2020 Tamas Chrenoczy-Nagy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
